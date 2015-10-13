8a0e0ba37fda08a33427f35ea84aba99859fdb1b
Cameron Moreau <cameron.cm6@gmail.com>
2015-10-13 00:03:48 -0500

    initializeBetterness

---
+++ README.md
@@ -0,0 +1,14 @@
+#MoreBetterJS
+It's JavaScript, but better.
+For NodeJS, obviously
+
+##Installing
+```
+bower install MoreBetterJS
+```
+
+##Code Example
+```javascript
+var mbjs = require('MoreBetterJS');
+mbjs.initializeBetterness();
+```
---
+++ morebetter.js
@@ -0,0 +1,17 @@
+/**
+ * MoreBetter.js: Like JavaScript, but better
+ * @author Cameron Moreau <cameron.cm6@gmail.com>
+ * @license MIT.
+ */
+
+(function() {
+    'use strict';
+
+    var Better = {};
+
+    Better.initializeBetterness = function() {
+        console.log('JavaScript is better');
+    };
+
+    module.exports = Better;
+}());
